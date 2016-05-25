4e5aa03f4d8f3e608b21dd8ffec29efcc34b07b9
Somnuk Wongkhan <somnuk.wk@gmail.com>
2016-05-25 20:57:34 +0700

    Test tap

--- app/about.component.ts
+++ app/about.component.ts
@@ -1,10 +1,12 @@
 
 
 import { Component } from "angular2/core";
+import { MDL } from "./mdl";
 
 @Component({
     selector: "app",
     templateUrl: "/app/about.component.html",
+    directives: [MDL]
 })
 export class AboutComponent {
 
